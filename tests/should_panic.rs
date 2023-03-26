3540a1a6152b18502e70664f188b88f6fd4b29e5
mWalrus <hellkvistoskar@protonmail.com>
2023-03-26 16:10:05 +0200

    feat: create a harnessless `should_panic` module for integration testing

---
+++ tests/should_panic.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use wally_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failure);
+    loop {}
+}
+
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+fn should_fail() {
+    serial_print!("should_panic::should_fail..........");
+    assert_eq!(0, 1);
+}
