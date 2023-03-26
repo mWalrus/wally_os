c3e8ab64c119ba7c05f61f25f929e8fa815ea9a2
mWalrus <hellkvistoskar@protonmail.com>
2023-03-26 15:55:02 +0200

    chore: adding integration tests
    This gives us the power to test the kernel with
    complete control over the environment, which makes
    it possible to test that code interacts correctly
    with the CPU or hardware devices.
    
    Some suggested future tests:
    - CPU Exceptions: When the code performs invalid
      operations (e.g., divides by zero), the CPU throws
      an exception. The kernel can register handler
      functions for such exceptions. An integration test
      could verify that the correct exception handler is
      called when a CPU exception occurs or that the
      execution continues correctly after a resolvable
      exception.
    - Page Tables: Page tables define which memory
      regions are valid and accessible. By modifying the
      page tables, it is possible to allocate new memory
      regions, for example when launching programs. An
      integration test could modify the page tables in
      the _start function and verify that the modifications
      have the desired effects in #[test_case] functions.
    - Userspace Programs: Userspace programs are
      programs with limited access to the system’s
      resources. For example, they don’t have access to
      kernel data structures or to the memory of other
      programs. An integration test could launch userspace
      programs that perform forbidden operations and verify
      that the kernel prevents them all.

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,29 @@
-// tell the kernel that we want to use our own custom test framework
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
 // don't include the standard library
 #![no_std]
 // removes the rust runtime
 #![no_main]
+// tell the kernel that we want to use our own custom test framework
+#![feature(custom_test_frameworks)]
+#![test_runner(wally_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use wally_os::println;
+
 // we have to implement our own panic handler since we no longer have access to the standard library
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
+    println!("[failed]\n");
+    println!("Error: {}\n", info);
     loop {}
 }
 
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}..........", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    use crate::exit::{exit_qemu, QemuExitCode};
-
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    wally_os::test_panic_handler(info)
 }
-
-#[macro_use]
-mod serial;
-#[macro_use]
-mod vga_buffer;
-mod exit;
 
 // no_mangle ensures that the compiler really outputs a function with the name "_start".
 // if we dont specify this, the compiler will mangle this name to something random
@@ -58,9 +34,6 @@ pub extern "C" fn _start() -> ! {
     // writer.write_string("Writing output with VGABuffer Writer");
     println!("Testing formatting: {} and {}", 42 + 18, 1.0 / 3.0);
     println!("Epic new line B)");
-
-    #[cfg(test)]
-    test_main();
 
     // panic!("This is a panic message!!!");
     // NOTE: uncomment when removing the panic above
