a4fc75c4bde6608afead2cd146e6164b27c8484a
mWalrus <hellkvistoskar@protonmail.com>
2023-03-26 01:29:28 +0100

    ref: re-organizing and commenting

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
+// don't include the standard library
 #![no_std]
 // removes the rust runtime
 #![no_main]
 
+use core::panic::PanicInfo;
+// we have to implement our own panic handler since we no longer have access to the standard library
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 mod vga_buffer;
 
-use core::panic::PanicInfo;
-
-use vga_buffer::Writer;
-
-static HELLO: &[u8] = b"Hello World!";
+use vga_buffer::VGAWriter;
 
 // no_mangle ensures that the compiler really outputs a function with the name "_start".
 // if we dont specify this, the compiler will mangle this name to something random
 #[no_mangle]
 // since we removed the rust runtime we need to define our own entry point.
+// `_start` is the default entry point on most systems.
 pub extern "C" fn _start() -> ! {
-    let mut writer = Writer::default();
+    let mut writer = VGAWriter::default();
     writer.write_string("Writing output with VGABuffer Writer");
-    loop {}
-}
-
-// we have to implement our own panic handler since we no longer have access to the standard library
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,7 @@
 // https://en.wikipedia.org/wiki/Code_page_437
+const BUFFER_WIDTH: usize = 80;
+const BUFFER_HEIGHT: usize = 25;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -22,11 +25,17 @@ pub enum Color {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// assures that the "newtype" has the exact same data structure as the underlying type.
+// https://doc.rust-lang.org/rust-by-example/generics/new_types.html
 #[repr(transparent)]
 struct ColorCode(u8);
 
 impl ColorCode {
     fn new(fg: Color, bg: Color) -> ColorCode {
+        // Let's say that the bg is LightGreen, i.e. 0xa/10u8, and we shift it 4 bits to
+        // the left, we get 0xa0/160u8.
+        // Let's also say that the fg color is Red, i.e. 0x4/4u8.
+        // We then bitwise OR the bg and fg to get the final color code 0xa4/164u8.
         ColorCode((bg as u8) << 4 | (fg as u8))
     }
 }
@@ -38,31 +47,29 @@ struct ScreenChar {
     color_code: ColorCode,
 }
 
-const BUFFER_WIDTH: usize = 80;
-const BUFFER_HEIGHT: usize = 25;
-
 #[repr(transparent)]
 struct Buffer {
     chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
-pub struct Writer {
+pub struct VGAWriter {
     column_position: usize,
     color_code: ColorCode,
     buffer: &'static mut Buffer,
 }
 
-impl Default for Writer {
+impl Default for VGAWriter {
     fn default() -> Self {
         Self {
             column_position: 0,
             color_code: ColorCode::new(Color::White, Color::Black),
+            // assign the buffer member a mutable pointer to the VGA text buffer
             buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
         }
     }
 }
 
-impl Writer {
+impl VGAWriter {
     pub fn write_byte(&mut self, byte: u8) {
         match byte {
             b'\n' => self.new_line(),
@@ -87,7 +94,9 @@ impl Writer {
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
             match byte {
+                // only write supported bytes
                 0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // for all other, unsupported bytes, print "■".
                 _ => self.write_byte(0xfe),
             }
         }
